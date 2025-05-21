6f851b7d5c5301434bd8777ee920f4de5d42fa13
Graffiti <martinbebesi09@gmail.com>
2025-05-21 13:08:09 +0200

    Add files via upload

--- 21_masodik.py
+++ 21_masodik.py
@@ -7,10 +7,10 @@ print("Két számot kapsz 1-10-ig. Késöbb több számot is Kérhetsz.")
 print("Ha túllépsz a 21-es számon akkor vesztettél!")
 print("Kezdjük el!")
 print("----------------------------------------")
-szamok = [random.randint(1, 10,), random.randint(1, 10)]
-jatekosok = str(input("Hányan játszotok? Ai/egyedül:"))
 def AI_game():
-    if jatekosok == "Ai":
+    szamok = [random.randint(1, 10, ), random.randint(1, 10)]
+    jatekosok = str(input("Hányan játszotok? Gép/egyedül:"))
+    if jatekosok == "Gép":
         print(f"Az első számod:{szamok[0]}")
         print(f"A második számod: {szamok[1]}")
 
@@ -18,27 +18,42 @@ def AI_game():
         szam_osszeg = sum(szamok)
         print(f"A számaid összege:{szam_osszeg}")
     summa()
-def ellenorzes():
-    if sum(szamok) == 21:
-        print("Nyertél")
-    elif sum(szamok) > 21:
-        print("Vesztettél!")
-    Ujra = str(input("Akarsz újra játszani?: Igen/Nem"))
-    if Ujra == "Igen":
-        AI_game()
 
+    def kartya_keres():
+        Valasztas = str(input("Ha kérsz még egy kártyát írj <Igen> ha nem akkor <Nem>"))
+        if Valasztas != "Igen" or "Nem":
+            print("Ez nem választási lehetőség")
+        if Valasztas == "Igen":
+            print("Akkor kapsz még kártyát!")
+        szamok.append(random.randint(1, 10))
+        print(f"A következő számod:{szamok[2]}")
+        summa()
+        ellenorzes()
+        kartya_keres()
 
+    def ellenorzes():
+        if sum(szamok) == 21:
+         print("Nyertél")
+        elif sum(szamok) > 21:
+            print("Vesztettél!")
+            Ujra = str(input("Akarsz újra játszani? Y/N:"))
+            if Ujra == "Y":
+                AI_game()
 
-def kartya_keres():
-    Valasztas = str(input("Ha kérsz még egy kártyát írj <Igen> ha nem akkor <Nem>"))
-    if Valasztas != "Igen" or "Nem":
-        print("Ez nem választási lehetőség")
-    if Valasztas == "Igen":
-        print("Akkor kapsz még kártyát!")
-    szamok.append(random.randint(1,10))
-    print(f"A következő számod:{szamok[2]}")
     kartya_keres()
+    def gép():
+        Gép_szamok = [random.randint(1, 10),random.randint(1,10)]
 
-kartya_keres()
+
+
+
+    def gép_summa():
+        Gép_osszeg = sum(Gép_szamok)
+    gép_summa()
+
+
+
+
+
 
 AI_game()
